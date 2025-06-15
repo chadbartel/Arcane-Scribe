3a7afc8ecc4dc49d8e193b659a6c93cbfd24fc69
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-15 16:45:47 -0700

    Add helpers module to extract cognito username from header

--- src/as-api-backend/api_backend/utils/__init__.py
+++ src/as-api-backend/api_backend/utils/__init__.py
@@ -7,10 +7,12 @@ This module provides helper functions that can be used across the API backend.
 from api_backend.utils.enums import AllowedMethod, ResponseSource
 from api_backend.utils.rag_query_processor import get_answer_from_rag
 from api_backend.utils.presigned_url_generator import generate_presigned_url
+from api_backend.utils.helpers import extract_username_from_basic_auth
 
 __all__ = [
     "get_answer_from_rag",
     "generate_presigned_url",
     "AllowedMethod",
     "ResponseSource",
+    "extract_username_from_basic_auth",
 ]
