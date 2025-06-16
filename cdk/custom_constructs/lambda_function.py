f4c2b6148a4836d7259ee6ee5473ba209de31b1a
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-16 07:32:04 -0700

    Change how the code path is calculated

--- cdk/custom_constructs/lambda_function.py
+++ cdk/custom_constructs/lambda_function.py
@@ -59,7 +59,9 @@ class CustomLambdaFromDockerImage(Construct):
 
         # Set variables for Lambda function
         name = os.path.basename(src_folder_path)
-        code_path = os.path.join(os.getcwd(), "src", src_folder_path)
+        code_path = os.path.join(
+            os.getcwd(), "src", f"{src_folder_path}.Dockerfile"
+        )
 
         # Append stack suffix to name if provided
         if stack_suffix:
@@ -86,10 +88,7 @@ class CustomLambdaFromDockerImage(Construct):
             function_name=name,
             runtime=lambda_.Runtime.FROM_IMAGE,
             handler=lambda_.Handler.FROM_IMAGE,
-            code=lambda_.Code.from_asset_image(
-                directory=code_path,
-                # This assumes a Dockerfile is present in the src folder
-            ),
+            code=lambda_.Code.from_asset_image(directory=code_path),
             memory_size=memory_size,
             timeout=timeout,
             environment=powertools_env_vars,
