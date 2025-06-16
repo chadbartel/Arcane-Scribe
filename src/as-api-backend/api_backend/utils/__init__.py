d410852bb1e72eaf6e54a0cf2dd11cfafdfec354
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-16 07:06:49 -0700

    Reapply black and isort formatting

--- src/as-api-backend/api_backend/utils/__init__.py
+++ src/as-api-backend/api_backend/utils/__init__.py
@@ -7,7 +7,7 @@ This module provides helper functions that can be used across the API backend.
 from api_backend.utils.enums import (
     AllowedMethod,
     ResponseSource,
-    DocumentProcessingStatus
+    DocumentProcessingStatus,
 )
 from api_backend.utils.rag_query_processor import get_answer_from_rag
 from api_backend.utils.presigned_url_generator import generate_presigned_url
