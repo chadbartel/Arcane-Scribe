003e1a049bc88bd24ef87bb7534716bb3b4028c4
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-19 09:44:20 -0700

    Remove unused presigned url request function and module

--- src/as-api-backend/api_backend/utils/__init__.py
+++ src/as-api-backend/api_backend/utils/__init__.py
@@ -5,9 +5,7 @@ This module provides helper functions that can be used across the API backend.
 
 # Local Modules
 from api_backend.utils.rag_query_processor import get_answer_from_rag
-from api_backend.utils.presigned_url_generator import generate_presigned_url
 
 __all__ = [
     "get_answer_from_rag",
-    "generate_presigned_url",
 ]
