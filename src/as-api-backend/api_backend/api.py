2efad0e13be46f05842180471b45b5b4094bf9fe
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-19 09:49:17 -0700

    Update api to include router for auth endpoint

--- src/as-api-backend/api_backend/api.py
+++ src/as-api-backend/api_backend/api.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter
 
 # Local Modules
-from api_backend.routers import query, srd
+from api_backend.routers import query, srd, auth
 
 # Create a router instance with a default prefix
 router = APIRouter()
@@ -10,3 +10,4 @@ router = APIRouter()
 # Include other routers
 router.include_router(query.router)
 router.include_router(srd.router)
+router.include_router(auth.router)
