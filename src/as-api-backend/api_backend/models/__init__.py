98a69ae23daffe4d12af9958018cc5de0fa0cb9b
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-25 12:47:43 -0700

    Update init import file with new data model

--- src/as-api-backend/api_backend/models/__init__.py
+++ src/as-api-backend/api_backend/models/__init__.py
@@ -10,7 +10,12 @@ from api_backend.models.srd import (
     PresignedUrlResponse,
     PresignedUrlErrorResponse,
 )
-from api_backend.models.auth import User, TokenResponse, SignUpRequest
+from api_backend.models.auth import (
+    User,
+    TokenResponse,
+    SignUpRequest,
+    RespondToChallengeRequest,
+)
 from api_backend.models.login import LoginRequest
 from api_backend.models.query import (
     RagQueryRequest,
@@ -31,4 +36,5 @@ __all__ = [
     "User",
     "TokenResponse",
     "SignUpRequest",
+    "RespondToChallengeRequest",
 ]
